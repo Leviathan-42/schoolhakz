7301c5a7703902a84b20fc37ab9e557f3c111a25
Leviathan-42 <levi@perry.lol>
2025-06-20 21:43:40 -0500

    Add files via upload

---
+++ uv.config.js
@@ -0,0 +1,13 @@
+// This file overwrites the stock UV config.js
+
+self.__uv$config = {
+  prefix: '/schoolhakz/service/',
+  bare: 'https://bare.benrogo.net/',
+  encodeUrl: (str) => btoa(str),
+  decodeUrl: (str) => atob(str),
+  handler: '/schoolhakz/uv/uv.handler.js',
+  bundle: '/schoolhakz/uv/uv.bundle.js',
+  client: '/schoolhakz/uv/uv.client.js',
+  config: '/schoolhakz/uv/uv.config.js',
+  sw: '/schoolhakz/uv/uv.sw.js',
+};
